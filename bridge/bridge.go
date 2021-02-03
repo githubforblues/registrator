6f641bcce5d36c11eb93b8f9e61d8b8c2cee4ea5
githubforblues <zhou_chaoyu@sina.cn>
2021-02-03 21:10:09 +0800

    代码调试

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -109,7 +109,7 @@ func (b *Bridge) Sync(quiet bool) {
 
 	log.Printf("Syncing services on %d containers", len(containers))
 
-	r, _ = json.Marshal(containers)
+	r, _ := json.Marshal(containers)
 	log.Printf("containers_j: %s", string(r))
 
 	// NOTE: This assumes reregistering will do the right thing, i.e. nothing..
